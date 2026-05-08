f73d0d3f784e27fea6eedcbc81b3f1e56020e80d
oltoit <otomaskovic@gmail.com>
2026-05-08 21:21:51 +0200

    man kann nachtichten schreiben??????

--- BadUX/src/views/LoginChatPage.vue
+++ BadUX/src/views/LoginChatPage.vue
@@ -1,16 +1,61 @@
 <script setup>
-    import { defineComponent } from 'vue';
     import ChatMessage from './chat-components/ChatMessage.vue';
+    import TextField from './chat-components/TextField.vue';
     import { useRoute } from "vue-router";
+    import { ref } from 'vue';
 
     const route = useRoute();
     const user = JSON.parse(route.query.user);
+
+    const messages = ref([]);
+    function onSend(message) {
+        messages.value.push(message);
+    }
 </script>
 
 <template>
     <div>
         <p>{{ user.name }}</p>
         <img :src="user.profilePicture" :alt="user.name">
-        <ChatMessage :message="'Ich werde alles zerstören'" :user="true"/>
+
+        <!--Message Container-->
+        <div class="MessageContainer">
+            <ChatMessage v-for="(message, index) in messages" :key="index" :message="message.message" :user="message.user"/>
+        </div>
+        
+
+        <div class="fick-mich-in-den-arsch">
+            <TextField class="unten" @send="onSend"/>
+            <div class="placeholder"></div>
+        </div>
     </div>
 </template>
+
+<style>
+    .unten {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .placeholder {
+        height: 50px;
+    }
+
+    .fick-mich-in-den-arsch {
+        display: flex;
+        flex-direction: column;
+        
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+
+    .MessageContainer {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        padding: 10px;
+        margin-bottom: 60px;
+    }
+</style>
