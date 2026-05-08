b5c9da979b739505f300cded2f06131a39b2f79e
oltoit <otomaskovic@gmail.com>
2026-05-08 19:55:28 +0200

    er holt daten

--- BadUX/src/router/index.ts
+++ BadUX/src/router/index.ts
@@ -4,11 +4,13 @@ import RegisterPage from '../views/RegisterPage.vue';
 import LoginPage from '../views/LoginPage.vue';
 import DeletePage from '../views/DeletePage.vue';
 import CaptchaPage from '../views/CaptchaPage.vue';
+import LoginChatPage from '../views/LoginChatPage.vue';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/register', component: RegisterPage },
   { path: '/login', component: LoginPage },
+  { path: '/login-chat', component: LoginChatPage },
   { path: '/delete', component: DeletePage },
   { path: '/captcha', component: CaptchaPage },
 ];
--- BadUX/src/views/LoginChatPage.vue
+++ BadUX/src/views/LoginChatPage.vue
@@ -1,18 +1,17 @@
-<template>
-  <div>
-    <h1>Bieeeeer</h1>
-    <img src="https://image.geo.de/30124472/t/Cx/v4/w1440/r0/-/bier-gezapft-f-154936574-jpeg--70436-.jpg" alt="">
-  </div>
-</template>
+<script>
+    import jsonData from '../users.json';
+    import { defineComponent } from 'vue';
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
-
-export default defineComponent({
-  name: 'LoginPage',
-});
+    export default defineComponent({
+        name: 'LoginChatPage',
+        data() {
+            return {
+                users: jsonData
+            }
+        }
+    });
 </script>
 
-<style scoped>
-
-</style>
+<template>
+    <p>{{ users[0].name }}</p>
+</template>
