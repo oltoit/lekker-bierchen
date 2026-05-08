3d79b4aa2b5f1364af80b7a51f4892ff106317e2
oltoit <otomaskovic@gmail.com>
2026-05-08 19:39:29 +0200

    crated person class

---
+++ BadUX/src/views/person.ts
@@ -0,0 +1,13 @@
+class Person {
+    name: string;
+    password: string;
+    biography: string;
+    profilePicture: string;
+
+    constructor(name: string, password: string, biography: string, profilePicture: string) {
+        this.name = name;
+        this.password = password;
+        this.biography = biography;
+        this.profilePicture = profilePicture;
+    }
+}
