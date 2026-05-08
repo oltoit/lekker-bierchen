b9b9e7b8fcceac171b102adc5fbe518b3446da51
oltoit <otomaskovic@gmail.com>
2026-05-08 20:43:35 +0200

    leerzeile eingefügt

--- BadUX/src/views/LoginChatPage.vue
+++ BadUX/src/views/LoginChatPage.vue
@@ -1,22 +1,10 @@
-<script>
+<script setup>
     import { defineComponent } from 'vue';
     import ChatMessage from './chat-components/ChatMessage.vue';
     import { useRoute } from "vue-router";
 
     const route = useRoute();
     const user = JSON.parse(route.query.user);
-
-    export default defineComponent({
-        name: 'LoginChatPage',
-        components: {
-            ChatMessage
-        },
-        data() {
-            return {
-                user: user
-            }
-        }
-    });
 </script>
 
 <template>
