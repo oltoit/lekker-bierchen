171494335e160b58dbc10d385527ba724ff18000
oltoit <otomaskovic@gmail.com>
2026-05-08 19:26:15 +0200

    ajbajbdhahsbdjhasbf

---
+++ BadUX/src/views/LoginChatPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Bieeeeer</h1>
+    <img src="https://image.geo.de/30124472/t/Cx/v4/w1440/r0/-/bier-gezapft-f-154936574-jpeg--70436-.jpg" alt="">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'LoginPage',
+});
+</script>
+
+<style scoped>
+
+</style>
