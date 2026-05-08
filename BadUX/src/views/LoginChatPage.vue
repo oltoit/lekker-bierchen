8591366e7e9693cc3b35ac971fb63d552133ddaf
oltoit <otomaskovic@gmail.com>
2026-05-08 20:21:11 +0200

    chat message

--- BadUX/src/views/LoginChatPage.vue
+++ BadUX/src/views/LoginChatPage.vue
@@ -1,9 +1,13 @@
 <script>
     import jsonData from '../users.json';
     import { defineComponent } from 'vue';
+import ChatMessage from './chat-components/ChatMessage.vue';
 
     export default defineComponent({
         name: 'LoginChatPage',
+        components: {
+            ChatMessage
+        },
         data() {
             return {
                 users: jsonData
@@ -13,5 +17,9 @@
 </script>
 
 <template>
-    <p>{{ users[0].name }}</p>
+    <div>
+        <p>{{ users[0].name }}</p>
+        <img :src="users[0].profilePicture" :alt="users[0].name">
+        <ChatMessage :message="'Ich werde alles zerstören'" :user="true"/>
+    </div>
 </template>
